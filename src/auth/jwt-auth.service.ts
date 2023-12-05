8ddc60e2ad47ccd96c91ea733a45c0b01a227b15
Ahmad Raza <farhan@findfarhan.me>
2023-12-05 17:52:56 +0500

    set code

--- src/auth/jwt-auth.service.ts
+++ src/auth/jwt-auth.service.ts
@@ -13,7 +13,7 @@ export class JwtAuthService {
   async generateToken(user: User): Promise<string> {
     const payload = { sub: user.id, name: user.email };
     const options = {
-      expiresIn: '1d', 
+      expiresIn: '30d', 
       secret: this.authConfig.jwt_secret, 
     };
 
